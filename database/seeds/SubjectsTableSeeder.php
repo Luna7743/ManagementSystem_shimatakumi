1f1b253bde9482bac7b400711dd2f4663996e766
島拓海 <>
2024-08-13 20:33:13 +0900

    ファイルを登録

---
+++ database/seeds/SubjectsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Users\Subjects;
+
+class SubjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 国語、数学、英語を追加
+        Subjects::create(['name' => '国語']);
+        Subjects::create(['name' => '数学']);
+        Subjects::create(['name' => '英語']);
+    }
+}
